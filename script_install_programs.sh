80f3aa22ea2a80fb683a31bee2fbfd6bee9e44aa
sufigueroa87 <sufigueroa87@gmail.com>
2022-12-04 01:00:24 +0100

    añado instalación librewolf

--- script_install_programs.sh
+++ script_install_programs.sh
@@ -54,6 +54,27 @@ echo "******************************************"
 snap install vlc
 
 
+echo "LIBREWOLF"
+echo "******************************************"
+echo "LIBREWOLF"
+echo "******************************************"
+echo "LIBREWOLF"
+echo "******************************************"
+# instalar librewolf
+distro=$(if echo " bullseye focal impish jammy uma una vanessa" | grep -q " $(lsb_release -sc) "; then echo $(lsb_release -sc); else echo focal; fi)
+  wget -O- https://deb.librewolf.net/keyring.gpg | gpg --dearmor -o /usr/share/keyrings/librewolf.gpg
+  tee /etc/apt/sources.list.d/librewolf.sources << EOF > /dev/null
+Types: deb
+URIs: https://deb.librewolf.net
+Suites: $distro
+Components: main
+Architectures: amd64
+Signed-By: /usr/share/keyrings/librewolf.gpg
+EOF
+apt update
+apt install librewolf -y
+
+
 echo "DRIVER WIFI"
 echo "******************************************"
 echo "DRIVER WIFI"
